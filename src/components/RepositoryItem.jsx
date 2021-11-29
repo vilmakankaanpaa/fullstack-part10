ea1b587aae709fdd3254122b700a6fb6f0d83a7c
Vilma Kankaanpää <vilma.kankaanpaa@aalto.fi>
2021-11-29 09:21:51 +0200

    Exercise 10.3

---
+++ src/components/RepositoryItem.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const RepositoryItem = ({ item }) => {
+
+
+  return (
+    <View>
+      <Text>Full name: {item.fullName}</Text>
+      <Text>Description: {item.description}</Text>
+      <Text>Language: {item.language}</Text>      
+      <Text>Forks: {item.forksCount}</Text>
+      <Text>Stars: {item.stargazersCount}</Text>
+      <Text>Rating: {item.ratingAverage}</Text>
+      <Text>Reviews: {item.reviewCount}</Text>
+    </View>
+  );
+};
+
+export default RepositoryItem;
