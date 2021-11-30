a8f9204d88c2ed4780c15c1fcc23b388c9e0582b
Vilma Kankaanpää <vilma.kankaanpaa@aalto.fi>
2021-11-30 10:13:38 +0200

    Exercise 10.11

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { FlatList, View, StyleSheet } from 'react-native';
+import { useQuery } from '@apollo/client';
+
 import RepositoryItem from './RepositoryItem';
+
+import { GET_REPOSITORIES } from '../graphql/queries';
 
 const styles = StyleSheet.create({
   separator: {
@@ -8,73 +12,34 @@ const styles = StyleSheet.create({
   },
 });
 
-const repositories = [
-  {
-    id: 'jaredpalmer.formik',
-    fullName: 'jaredpalmer/formik',
-    description: 'Build forms in React, without the tears',
-    language: 'TypeScript',
-    forksCount: 1589,
-    stargazersCount: 21553,
-    ratingAverage: 88,
-    reviewCount: 4,
-    ownerAvatarUrl: 'https://avatars2.githubusercontent.com/u/4060187?v=4',
-  },
-  {
-    id: 'rails.rails',
-    fullName: 'rails/rails',
-    description: 'Ruby on Rails',
-    language: 'Ruby',
-    forksCount: 18349,
-    stargazersCount: 45377,
-    ratingAverage: 100,
-    reviewCount: 2,
-    ownerAvatarUrl: 'https://avatars1.githubusercontent.com/u/4223?v=4',
-  },
-  {
-    id: 'django.django',
-    fullName: 'django/django',
-    description: 'The Web framework for perfectionists with deadlines.',
-    language: 'Python',
-    forksCount: 21015,
-    stargazersCount: 48496,
-    ratingAverage: 73,
-    reviewCount: 5,
-    ownerAvatarUrl: 'https://avatars2.githubusercontent.com/u/27804?v=4',
-  },
-  {
-    id: 'reduxjs.redux',
-    fullName: 'reduxjs/redux',
-    description: 'Predictable state container for JavaScript apps',
-    language: 'TypeScript',
-    forksCount: 13902,
-    stargazersCount: 52869,
-    ratingAverage: 0,
-    reviewCount: 0,
-    ownerAvatarUrl: 'https://avatars3.githubusercontent.com/u/13142323?v=4',
-  },
-];
-
 const ItemSeparator = () => <View style={styles.separator} />;
 
 const RepositoryList = () => {
 
+  const result = useQuery(GET_REPOSITORIES, {
+    fetchPolicy: 'cache-and-network',
+  });    
+
+  let repositoryNodes = [];
+  if (result.data) {
+    repositoryNodes = result.data.repositories.edges.map(item => item.node);
+    console.log(repositoryNodes);
+  }
+
   const renderItem = ({ item }) => {
     return (
-      <RepositoryItem
+      <RepositoryItem key={item.id}
         item={item}
       />
     );
   };
 
   return (
-    <View >
-      <FlatList
-        data={repositories}
-        ItemSeparatorComponent={ItemSeparator}
-        renderItem={renderItem}
-      />
-    </View>
+    <FlatList
+      data={repositoryNodes}
+      ItemSeparatorComponent={ItemSeparator}
+      renderItem={renderItem}
+    />
   );
 };
 
